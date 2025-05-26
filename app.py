8982aee21f0338d3365a3c825b16d38ab7d61ff4
Shani <shanihlli1234@gmail.com>
2025-05-26 20:11:41 +0300

    app.py - for vercel

--- app.py
+++ app.py
@@ -4,22 +4,15 @@ from mongodb_connection_manager import MongoConnectionManager
 from routes import initial_routes
 
 import os
+
 app = Flask(__name__)
 Swagger(app)
-
-#invilaze Database connection
 MongoConnectionManager.initialize_db()
-
-
-#import the routes
 initial_routes(app)
 
 @app.route('/')
 def index():
     return "Welcome to Ad SDK Flask API!"
 
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8088))
-    app.run(debug=True , port=port)
+# ⛔️ חשוב! לא צריך app.run() כשמריצים על Vercel
+# זה כן רץ מקומית, אבל לא בשירותים serverless
